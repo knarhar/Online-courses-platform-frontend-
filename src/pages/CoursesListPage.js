c2b5ea59dfbddcaf8c50d57e88ae765b1172cf3b
Knarik <harutyunyanqnarik2@gmail.com>
2023-12-07 22:24:32 +0400

    CoursesListPage image displaying

--- src/pages/CoursesListPage.js
+++ src/pages/CoursesListPage.js
@@ -1,32 +1,36 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 
-function CoursesListPage () {
+function CoursesListPage() {
+  const [courses, setCourses] = useState([]);
 
-    let [ courses, setCourses] = useState([]) 
+  useEffect(() => {
+    getCourses();
+  }, []);
 
-    useEffect(()=>{
-        getCourses()
-    }, [])
-
-    let getCourses = async ()=>{
-        let response = await fetch('http://127.0.0.1:8000/api/courses/')
-        let data = await response.json()
-        setCourses(data)
+  const getCourses = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/courses/');
+      const data = await response.json();
+      setCourses(data);
+    } catch (error) {
+      console.error('Error fetching courses:', error);
     }
-
+  };
+  console.log(courses)
   return (
     <div>
-        <div className='courses-list'>
-            {courses.map((course, index)=>(
-                <div key={index} className='course-card'>
-                    <h3>{course.title}</h3>
-                    <p>{course.description}</p>
-                    <p>{course.is_paid}</p>
-                </div>
-            ))}
-        </div>
+      <div className='courses-list'>
+        {courses.map((course, index) => (
+          <div key={index} className='course-card'>
+            <h3>{course.title}</h3>
+            <img src={course.pic} alt={`Course ${course.title}`} />
+            <p>{course.description}</p>
+            <p>{course.is_paid ? 'Paid' : 'Free'}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default CoursesListPage
+export default CoursesListPage;
